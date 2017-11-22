ea78b54505ea238461a74bfe7da788b4fc776b81
AppVeyor <full-build@appveyor.com>
2017-11-22 19:45:17 +0100

    implement mandatory repositories

--- sbs/Commands/Workspace.fs
+++ sbs/Commands/Workspace.fs
@@ -8,5 +8,18 @@ let InitWorkspace (cmd : CLI.Commands.InitWorkspace) =
     // first clone master repository
     let masterRepo = { Configuration.Master.Repository.Name = ".sbs"
                        Configuration.Master.Repository.Uri = Helpers.Env.MasterRepository () }
-    Core.Git.GitClone masterRepo wsDir "master" true
+    Core.Git.GitClone masterRepo wsDir "master" false
         |> Helpers.IO.CheckResponseCode
+
+    let currentDir = System.Environment.CurrentDirectory
+    try
+        System.Environment.CurrentDirectory <- wsDir.FullName
+
+        // delegate to clone then to get dependencies
+        let cloneRepo = { CLI.Commands.CloneRepository.Name = masterRepo.Name
+                          CLI.Commands.CloneRepository.Shallow = false
+                          CLI.Commands.CloneRepository.Dependencies = true 
+                          CLI.Commands.CloneRepository.References = false }
+        cloneRepo |> Commands.Sources.Clone
+    finally
+        System.Environment.CurrentDirectory <- currentDir
