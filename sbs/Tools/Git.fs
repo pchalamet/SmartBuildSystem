56e5aab9eba226ec61bee6b3514cdd0160697793
AppVeyor <full-build@appveyor.com>
2017-11-27 10:04:15 +0100

    no need to create folder

--- sbs/Tools/Git.fs
+++ sbs/Tools/Git.fs
@@ -7,11 +7,11 @@ open Helpers.Collections
 let Clone (repo : Configuration.Master.Repository) (wsDir : DirectoryInfo) (shallow : bool) =
     let bronly = "--no-single-branch"
     let depth = shallow ? ("--depth=1", "")
-    let targetDir = wsDir |> GetDirectory repo.Name |> EnsureExists
+    let targetDir = wsDir |> GetDirectory repo.Name
     let args = sprintf @"clone %s --quiet %s %s %A" repo.Uri bronly depth targetDir.FullName
     ExecGetOutput "git" args wsDir Map.empty
 
 let Checkout (repo : Configuration.Master.Repository) (wsDir : DirectoryInfo) (version : string) =
     let args = sprintf "checkout %A" version
-    let targetDir = wsDir |> GetDirectory repo.Name |> EnsureExists
+    let targetDir = wsDir |> GetDirectory repo.Name
     ExecGetOutput "git" args targetDir Map.empty
