8f030517d0b16f44dc120951b98e159825cdcf1c
Pierre Chalamet <pchalamet@smartadserver.com>
2017-12-18 14:08:27 +0100

    force msbuild if not found

--- build.cmd
+++ build.cmd
@@ -1,5 +1,10 @@
+setlocal
+set PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\bin\amd64
 set HERE=%~dp0
+
 set CONFIG=%1
+if "%CONFIG%" == "" set CONFIG=Release
+
 %HERE%.paket\paket.exe restore
 msbuild /t:Build /p:Configuration=%CONFIG% %HERE%SmartBuildSystem.sln
 %HERE%packages\7-Zip.CommandLine\tools\7za.exe a -r artifacts\sbs.zip sbs\bin\%CONFIG%\*
