a0256ca05d0b536d916540bcbf6c94ecab19db0e
Pierre Chalamet <pchalamet@smartadserver.com>
2018-02-07 15:00:13 +0100

    publish to nuget

--- build.cmd
+++ build.cmd
@@ -3,8 +3,14 @@ set PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\M
 set HERE=%~dp0
 
 set CONFIG=%1
+set VERSION=%2
 if "%CONFIG%" == "" set CONFIG=Release
+if "%VERSION%" == "" set VERSION=0.0.0
+
+if exist %HERE%artifacts rmdir /q /s %HERE%artifacts
 
 %HERE%.paket\paket.exe restore
 msbuild /t:Build /p:Configuration=%CONFIG% %HERE%SmartBuildSystem.sln
 %HERE%packages\7-Zip.CommandLine\tools\7za.exe a -r artifacts\sbs.zip .\sbs\bin\%CONFIG%\*
+%HERE%packages\NuGet.CommandLine\tools\NuGet.exe pack sbs.nuspec -OutputDirectory %HERE%artifacts -Version %VERSION% -Properties Configuration=%CONFIG%
+move artifacts\sbs.%VERSION%.nupkg artifacts\sbs.nupkg
