bc849fedf47c29e355db05783f5175579fb2fdce
Pierre Chalamet <pchalamet@smartadserver.com>
2018-02-13 23:15:47 +0100

    refactor depedency scan

--- sbs/Core/View.fs
+++ sbs/Core/View.fs
@@ -3,18 +3,29 @@ open System.IO
 open Helpers
 open Helpers.Collections
 open Core.Repository
+open System.Collections.Generic
 
 
-let rec private gatherDependencies wsDir (config : Configuration.Master.Configuration) (closure : Configuration.Master.Repository set) =
-    let getDependencies (repo : Configuration.Master.Repository) (state : Configuration.Master.Repository set) =
-        let newDependencies = { RepositoryName = repo.Name }.FindDependencies wsDir config
-        state + newDependencies
 
-    let repoToGather = closure |> Set.fold (fun s t -> getDependencies t s) closure
-    if repoToGather <> closure then gatherDependencies wsDir config repoToGather
-    else closure
+let private gatherRepoDependencies wsDir (config : Configuration.Master.Configuration) (repo : Configuration.Master.Repository) =
+    { RepositoryName = repo.Name }.FindDependencies wsDir config |> Set.toSeq
 
-    
+let rec private gatherDependencies wsDir (config : Configuration.Master.Configuration) (repos : Configuration.Master.Repository set) (processedRepos : Configuration.Master.Repository set) =
+    if repos <> Set.empty then
+        printfn "Scanning %s" (System.String.Join(", ", repos |> Seq.map (fun x -> x.Name)))
+
+        let deps = repos |> Seq.fold (fun s t -> s |> Map.add t (gatherRepoDependencies wsDir config t)) Map.empty
+        let newRepos = deps |> Seq.map (fun x -> x.Value)
+                            |> Seq.concat
+                            |> Set
+
+        let newProcessedRepos = processedRepos + repos
+        let unprocessedRepos = newRepos - newProcessedRepos
+        deps |> Map.union (gatherDependencies wsDir config unprocessedRepos newProcessedRepos)
+    else
+        Map.empty
+
+
 let private gatherProjects wsDir (repo : Configuration.Master.Repository) =
     let repoDir = wsDir |> Fs.GetDirectory repo.Name
     let enumerateExtensions ext = repoDir.EnumerateFiles("*" + ext, SearchOption.AllDirectories)
@@ -23,27 +34,31 @@ let private gatherProjects wsDir (repo : Configuration.Master.Repository) =
 
 
 
-
 type View =
     { Name : string
       Patterns : string set
-      Dependencies : bool
-      Repositories : Configuration.Master.Repository set 
+      WithDependencies : bool
+      Dependencies : Map<Configuration.Master.Repository, Configuration.Master.Repository seq>
       Projects : string set }
 with
-    static member Materialize (wsDir : DirectoryInfo) (config : Configuration.Master.Configuration) (name : string) (patterns : string set) (dependencies : bool) =
+    static member Materialize (wsDir : DirectoryInfo) (config : Configuration.Master.Configuration) (name : string) (patterns : string set) (withDependencies : bool) =
         let selectedRepos = Helpers.Text.FilterMatch (config.Repositories) (fun x -> x.Name) patterns
-        let repos = if dependencies then gatherDependencies wsDir config selectedRepos
-                    else selectedRepos
+        let dependencies = if withDependencies then gatherDependencies wsDir config selectedRepos Set.empty
+                           else selectedRepos |> Seq.map (fun x -> x, Seq.empty) |> Map
+        let repos = dependencies |> Seq.map (fun x -> x.Key)
         let projects = repos |> Seq.fold (fun s t -> Seq.append s (gatherProjects wsDir t)) Seq.empty
                              |> Seq.map (fun x -> x.FullName)
                              |> Set.ofSeq
         { Name = name
           Patterns = patterns
+          WithDependencies = withDependencies
           Dependencies = dependencies
-          Repositories = repos
           Projects = projects }
 
     member this.Save (wsDir : DirectoryInfo) =
         let viewFile = wsDir |> Fs.GetFile (sprintf "%s.view" this.Name)
-        File.WriteAllLines(viewFile.FullName, this.Repositories |> Seq.map (fun x -> x.Name))
+        let lines = this.Dependencies |> Seq.map (fun x -> let deps = x.Value |> Seq.map (fun x -> x.Name)
+                                                           let sdeps = System.String.Join(", ", deps)
+                                                           sprintf "%s <- %s" x.Key.Name sdeps)
+                                                           
+        File.WriteAllLines(viewFile.FullName, lines)
--- sbs/Helpers/Collections.fs
+++ sbs/Helpers/Collections.fs
@@ -9,3 +9,8 @@ let (?) (q: bool) (yes: 'a, no: 'a) = if q then yes else no
 type Set<'T when 'T : comparison>
 with
     static member substract a b = Set.difference b a
+
+type Map<'v, 'k when 'v : comparison>
+with
+    static member union (p:Map<'a,'b>) (q:Map<'a,'b>) = 
+        Map.fold (fun acc key value -> acc |> Map.add key value) p q
